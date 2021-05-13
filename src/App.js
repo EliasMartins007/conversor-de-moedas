327072a18991d700c042dd5a33c6817492f007e9
elias <eliasfaculdade@yahoo.com.br>
2021-05-13 17:40:02 -0300

    subindo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useState } from 'react';
+import './styles.css';
+import Button from '@material-ui/core/Button';
 
-function App() {
+export default function App() {
+  const [currencyA] = useState('USD');
+  const [currencyB] = useState('BRL');
+
+  const [currencyAValue, setCurrencyAValue] = useState(0);
+  const [currencyBValue, setCurrencyBValue] = useState(0);
+
+  const converter = useCallback(() => {
+    const from_to = `${currencyA}_${currencyB}`;
+    const url = `https://free.currconv.com/api/v7/convert?q=${from_to}&compact=ultra&apiKey=2e2282f1c1fd83c440ff`;
+
+    fetch(url)
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        const cotacao = json[from_to];
+
+        const conversao = (cotacao * parseFloat(currencyAValue)).toFixed(2);
+
+        setCurrencyBValue(conversao);
+      });
+  }, [currencyA, currencyB, currencyAValue]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>
+        {currencyA} para {currencyB}
+      </h2>
+      <input
+        type="number"
+        min="0.01"
+        step="0.01"
+        placeholder={currencyA}
+        onChange={(e) => setCurrencyAValue(e.target.value)}
+      />
+      <Button variant="contained" color="primary" onClick={converter}>
+        Conversor
+      </Button>
+      <h2>{currencyBValue}</h2>
     </div>
   );
 }
-
-export default App;
